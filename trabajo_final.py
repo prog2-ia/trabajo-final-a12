bbf23ebdaa58bde6adf94f0bc50bcd39124e64a8
David <dam70@alu.ua.es>
2026-02-22 23:48:10 +0100

    Creacion de la clase Deuda

--- trabajo_final.py
+++ trabajo_final.py
@@ -1,3 +1,10 @@
-# añadiendo un commit
-for i in range(0,6):
-    print(i)
+class Deuda:
+    def __init__(self,cantidad_deuda,interes,pago_mensual):
+        self.cantidad_deuda=cantidad_deuda
+        self.interes = interes
+        self.pago_mensual=pago_mensual
+    def deuda_restante(self,numero_pagos):
+        self.numero_pagos = numero_pagos
+        return self.cantidad_deuda - (self.pago_mensual*self.numero_pagos)
+casa = Deuda(100_000,0.05,650)
+print(casa.deuda_restante(5))
