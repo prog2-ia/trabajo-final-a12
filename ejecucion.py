6677222f44f37a8c7234577ed52f158c44ba45e2
David <dam70@alu.ua.es>
2026-03-02 12:15:23 +0100

    creando el fichero main

---
+++ ejecucion.py
@@ -0,0 +1,9 @@
+from cuenta_bancaria import CuentaBancaria
+from agente_financiero import AgenteFinanciero
+from Deuda import Deuda
+if __name__ == '__main__':
+    1234_6574_2345_5674 = CuentaBancaria("David Arenas Martínez")
+    David = AgenteFinanciero("David")
+    reytf = Deuda("25678901Y",100_000)
+
+
