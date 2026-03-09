df513aab296d5b1f8daab7034a48fb0e062e7313
David <dam70@alu.ua.es>
2026-03-09 14:06:00 +0100

    Sustituyo dinero_ahorrado para conectarlo con CuentaBancaria

--- familiar.py
+++ familiar.py
@@ -1,15 +1,14 @@
 class Familiar:
-    def __init__(self, nombre,apellido1,apellido2,edad,gastos_fijos,paga=0,dinero_ahorrado=0):
+    def __init__(self, nombre,apellido1,apellido2,edad,gastos_fijos,paga=0,cuenta):
         self.nombre = nombre
         self.apellido1 = apellido1
         self.apellido2 = apellido2
         self.edad = edad
         self.gastos_fijos = gastos_fijos
         self.paga = paga
-        self.dinero_ahorrado=dinero_ahorrado
+        self.cuenta = cuenta
     def comprar(self,valor_de_compra,producto):
-        self.valor_de_compra = valor_de_compra
-        self.producto=producto
-        if self.valor_de_compra >= self.dinero_ahorrado:
-            self.dinero_ahorrado-=self.valor_de_compra
-            return (f"Has comprado {self.producto} te quedan {self.dinero_ahorrado}")
+        if self.cuenta.retirar(valor_de_compra): # if true entonces
+            return (f"Has comprado {producto}, te quedan {self.cuenta.saldo}€ en la cuenta.")
+        else:
+            return (f"No tienes saldo suficiente para comprar {producto}.")
