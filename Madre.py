b93b2a11ba6ae0fb71a5678f8d53d6c87a2592ad
David <dam70@alu.ua.es>
2026-03-16 12:42:17 +0100

    Ordeno el salario y la cuenta en el innit ya que me daba eeor de ejecucion y borro el atributo dinero ahorrado

--- Madre.py
+++ Madre.py
@@ -1,9 +1,9 @@
 from Adulto import Adulto
 class Madre(Adulto):
-    def __init__(self, nombre, apellido1, apellido2, edad, gastos_fijos, salario, dinero_ahorrado=0):
-        super().__init__(nombre, apellido1, apellido2, edad, gastos_fijos, salario, dinero_ahorrado)
+    def __init__(self, nombre, apellido1, apellido2, edad, gastos_fijos, cuenta,salario ):
+        super().__init__(nombre, apellido1, apellido2, edad, gastos_fijos, cuenta, salario)
 
     def generar_intereses(self, porcentaje=0.02):
-        intereses = self.dinero_ahorrado * porcentaje
+        intereses = self.dinero_ahorrado * porcentaje #variable local
         self.dinero_ahorrado += intereses
         return f"Intereses generados: {intereses:.2f}€. "
