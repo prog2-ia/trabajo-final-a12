0b718b68b5e31a050612de1b50eff550a3252aae
David <dam70@alu.ua.es>
2026-03-16 12:24:11 +0100

    creo el metodo realizar inversión, actualizo el metodo realizar transaccion y heredo de la clase adulto ya que un agente financiero es un adulto

--- agente_financiero.py
+++ agente_financiero.py
@@ -1,11 +1,28 @@
-class AgenteFinanciero:
-    moneda = 'USD'
-    def __init__(self,nombre,saldo_inicial):
-        self.nombre = nombre
-        self.presupuesto = saldo_inicial
-    def realizar_transaccion(self):
-        if CuentaBancaria.cantidad <= self.presupuesto:
-            self.presupuesto -= CuentaBancaria.cantidad
-            print(f"{self.nombre} gastó {CuentaBancaria.cantidad} {AgenteFinanciero.moneda}")
+import random
+from Adulto import Adulto
+class AgenteFinanciero(Adulto):
+    inversion =[0.25,0.5,1,2,4]
+    moneda = '€'
+    def __init__(self, nombre, apellido1, apellido2, edad, gastos_fijos, cuenta, salario):
+        super().__init__(nombre, apellido1, apellido2, edad, gastos_fijos, cuenta, salario)
+    def realizar_transaccion(self, cantidad):
+        if self.cuenta.retirar(cantidad): # llamamos al metodo retirar que nos devolvera true si tiene fondos
+            # en caso de que no tenga nos devolvera false por lo que pasara al else
+            print(f"Transacción de {cantidad}{self.moneda} realizada con éxito.")
+            print(f"Saldo actual de {self.nombre}: {self.cuenta.saldo}{self.moneda}")
+            return True
         else:
-            print("Fondos insuficientes")
+            print(f"Fondos insuficientes en la cuenta de {self.nombre}.")
+            return False
+    def realizar_inversión(self,cantidad):
+        if self.cuenta.retirar(cantidad):
+            resultado = random.choice(self.inversion) * cantidad
+            self.cuenta.ingresar(resultado)
+            if resultado >= cantidad:
+                print(f"Has invertido {cantidad} y has conseguido {resultado - cantidad} {self.moneda} de beneficio"
+                      f" te has quedado con {resultado} {self.moneda} respecto a tu inversión inicial" )
+            else:
+                print(f"Has invertido {cantidad} y has perdido {cantidad - resultado} {self.moneda}"
+                      f" te has quedado con {resultado} {self.moneda} respecto a tu inversión inicial")
+        else:
+            print("No tienes esa cantidad de dinero para invertir")
