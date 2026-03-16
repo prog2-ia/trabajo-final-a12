56c5d8b4aaf10dc35b84d16b3d56d7d415f128d3
David <dam70@alu.ua.es>
2026-03-16 13:13:26 +0100

    Haciendo la segunda versión de fichero de ejecucuion y pruebo las nuevas clases

--- ejecucion.py
+++ ejecucion.py
@@ -1,9 +1,24 @@
 from cuenta_bancaria import CuentaBancaria
 from agente_financiero import AgenteFinanciero
 from Deuda import Deuda
+from Adulto import Adulto
+from Ahorro import Ahorro
+from agente_financiero import AgenteFinanciero
+from madre_asesora import MadreAsesora
 if __name__ == '__main__':
-    1234_6574_2345_5674 = CuentaBancaria("David Arenas Martínez")
-    David = AgenteFinanciero("David")
-    reytf = Deuda("25678901Y",100_000)
+    prueba_cuenta=CuentaBancaria("renato",30_000)
+    renato = Adulto("renato","sanchez","muñoz",41,550,prueba_cuenta,1800)
+    vacaciones= Ahorro("vacaciones de verano",2000)
+    vacaciones.aportar(prueba_cuenta,23.3)
+    print(renato.cobrar_nomina())
+    print(renato.comprar(1000,"frigorifico"))
+    cuenta_juan = CuentaBancaria("Juan Perez", 10000)
+    agente = AgenteFinanciero("Juan","Perez","García",40,500,cuenta_juan,2500)
+    print(agente.cobrar_nomina())
+    agente.realizar_transaccion(200)
+    agente.realizar_inversión(300)
+    mi_cuenta = CuentaBancaria("Ana López", 5000)
+    ana = MadreAsesora("Ana", "López", "García", 42, 800, mi_cuenta, 3000)
+    ana.realizar_inversión(1000)
 
 
