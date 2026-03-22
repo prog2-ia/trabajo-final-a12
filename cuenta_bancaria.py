d8e311b63e682dd0a9ea6099b3b5c3fb14369abb
David <dam70@alu.ua.es>
2026-03-22 16:53:04 +0100

    Hago el saldo atributo privado y creo un getter y  un setter para acceder de forma segura

--- cuenta_bancaria.py
+++ cuenta_bancaria.py
@@ -1,12 +1,20 @@
 class CuentaBancaria:
     def __init__(self, titular, saldo_inicial=0):
         self.titular = titular
-        self.saldo = saldo_inicial
-
+        self.__saldo = saldo_inicial
+    @property
+    def saldo(self):
+        return self.__saldo
+    @saldo.setter
+    def saldo(self, nueva_cantidad):
+        if nueva_cantidad >= 0:
+            self.__saldo = nueva_cantidad
+        else:
+            print("Error: El saldo no puede ser negativo.")
     def ingresar(self, cantidad):
-        self.saldo += cantidad
+        if cantidad > 0:
+            self.saldo += cantidad
         return self.saldo
-
     def retirar(self, cantidad):
         if self.saldo >= cantidad:
             self.saldo -= cantidad
