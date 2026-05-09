d4a67c16b4724ddcc341e1c88721a13e5e3d9f30
eriiic16 <ecd26@alu.ua.es>
2026-05-10 01:30:28 +0200

    creación inicial de main.py y menú de navegación básico

---
+++ main.py
@@ -0,0 +1,19 @@
+from cuenta_bancaria import CuentaBancaria
+from Adulto import Adulto
+
+def menu():
+    print("BIENVENIDO AL SISTEMA FAMILIAR")
+    # Creamos una cuenta y un usuario de prueba
+    cuenta = CuentaBancaria("Familia Test", 500)
+    usuario = Adulto("Juan", "Perez", "Gomez", 40, 600, cuenta, 1500)
+
+    while True:
+        print("\n1. Ver Saldo\n2. Salir")
+        opcion = input("Selecciona: ")
+        if opcion == "1":
+            print(f"Saldo: {usuario.cuenta.saldo}") # Acceso a cuenta
+        elif opcion == "2":
+            break
+
+if __name__ == "__main__":
+    menu()
